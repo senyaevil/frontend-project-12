d7fa830770126aa1ab8ce3ccee11a1785c53a2a2
senyaevil <60550469+senyaevil@users.noreply.github.com>
2025-12-18 16:55:16 +0300

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -6,26 +6,26 @@ const socketInit = (store) => {
   const socket = io()
   const { channelsApi, messagesApi } = Api
 
-  socket.on('newMessage', payload => {
+  socket.on('newMessage', (payload) => {
     store.dispatch(messagesApi.util.updateQueryData('getMessages', undefined, (draftMessages) => {
       draftMessages.push(payload)
     }))
   })
 
-  socket.on('newChannel', payload => {
+  socket.on('newChannel', (payload) => {
     store.dispatch(channelsApi.util.updateQueryData('getChannels', undefined, (draftChannels) => {
       draftChannels.push(payload)
     }))
   })
 
-  socket.on('renameChannel', payload => {
+  socket.on('renameChannel', (payload) => {
     store.dispatch(channelsApi.util.updateQueryData('getChannels', undefined, (draftChannels) => {
       const channel = draftChannels.find((item) => item.id === payload.id)
       channel.name = payload.name
     }))
   })
 
-  socket.on('removeChannel', payload => {
+  socket.on('removeChannel', (payload) => {
     store.dispatch(channelsApi.util.updateQueryData('getChannels', undefined, (draftChannels) => {
       const newChannels = draftChannels.filter((channel) => channel.id !== payload.id)
       const state = store.getState()
